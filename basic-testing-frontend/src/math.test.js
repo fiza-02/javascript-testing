473e8ca7569cd1b65f34b93d7754a8bb8684df2a
Mohammed Yasmin <mdyasmin0210@gmail.com>
2023-11-22 18:58:42 +0530

    AAA pattern

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,11 +1,18 @@
 // #expect  for expecation
 // toBe is  a function available in assertion library.
-import {test, expect} from 'vitest';
+import { test, expect } from 'vitest';
 import { add } from './math';
 
 
 //test(name, fn, timeout)
-test('should summarize all number values in an aaray',()=>{
-const result= add([1,2,3]);
-expect(result).toBe(6);
+test('should summarize all number values in an aaray', () => {
+    // Arrange
+    const numbers = [1, 2];
+
+    // Add
+    const result = add([1, 2, 3]);
+
+    // Assert
+    const expectedResult = numbers.reduce((prevValue, curValue) => prevValue + curValue,0);
+    expect(result).toBe(expectedResult);
 })
