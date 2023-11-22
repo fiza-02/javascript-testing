cd2a5639bf6bca1049cac91fc70e12300828d203
Mohammed Yasmin <mdyasmin0210@gmail.com>
2023-11-22 19:12:23 +0530

    adding more tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -10,9 +10,34 @@ test('should summarize all number values in an aaray', () => {
     const numbers = [1, 2];
 
     // Add
-    const result = add([1, 2, 3]);
+    const result = add(numbers);
 
     // Assert
     const expectedResult = numbers.reduce((prevValue, curValue) => prevValue + curValue,0);
+    expect(result).toBe(expectedResult);
+});
+
+//test(name, fn, timeout)
+test('should yield NaN if a least one invalid number is provided', () => {
+    // Arrange
+    const numbers = ['invalid', 2];
+
+    // Add
+    const result = add(numbers);
+
+    // Assert
+    expect(result).toBeNaN();
+})
+
+//test(name, fn, timeout)
+test('should yield a correct sum if an array of numeric string values is provided', () => {
+    // Arrange
+    const numbers = ['1', '2'];
+
+    // Add
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers.reduce((prevValue, curValue) => +prevValue + +curValue,0);
     expect(result).toBe(expectedResult);
 })
