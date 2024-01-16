b2b87fac8ae38b98c92e01f070f855dad434df5a
Mohammed Yasmin <mdyasmin0210@gmail.com>
2024-01-16 13:27:33 +0530

    refactoring code for testing single thing or feature

---
+++ basic-testing-frontend/src/output.js
@@ -0,0 +1,9 @@
+export function generateResult(result){
+    let resultText = '';
+  if (result === 'invalid') {
+    resultText = 'Invalid input. You must enter valid numbers.';
+  } else if (result !== 'no-calc') {
+    resultText = 'Result: ' + result;
+  }
+  return resultText;
+}
